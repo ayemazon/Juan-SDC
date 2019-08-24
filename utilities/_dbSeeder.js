1caaf92a9c039e1e7181a1502fdb39689e057185
Juan Gonzalez <juangonzalez@juans-mbp.home>
2019-08-24 16:59:59 -0400

    Fix Comments

--- utilities/_dbSeeder.js
+++ utilities/_dbSeeder.js
@@ -24,47 +24,14 @@ const generateFakeData = (i) => {
 };
 
 const _dbSeeder = (i) => {
-  /*
-  let fakeInfoArr = [];
-  let counter = 0;
-  while (counter < 100) {
-    fakeInfoArr.push({
-      id: counter + 1,
-      title: faker.fake('{{commerce.productName}}'),
-      description: faker.fake('{{lorem.lines}}'),
-      product_price: faker.fake('{{commerce.price}}'),
-      seller: faker.fake('{{internet.userName}}'),
-      colors: (() => {
-        let resultArr = [];
-        let numberOfColors = Math.floor(Math.random() * Math.floor(4));
-        let colorCount = 0;
-        while (colorCount <= numberOfColors) {
-          resultArr.push(faker.fake('{{commerce.color}}'));
-          colorCount++;
-        }
-        return resultArr;
-      })(),
-    });
-    counter++;
-  }
-  */
-
-  // let fakeInfoArr = [];
   let counter = 0;
   while (counter < 500000) {
-    // fakeInfoArr.push([
       addToDatabase(generateFakeData());
     counter++;
     if (counter % 10000 === 0) {
       console.log('Count at: ', counter);
     }
   }
-
-  // console.log('ARRAY', fakeInfoArr);
-
-  // fakeInfoArr.forEach((fakeInfo) => {
-  //   addToDatabase(fakeInfo)
-  // });
   console.log("Seeding Done");
 };
  _dbSeeder();
